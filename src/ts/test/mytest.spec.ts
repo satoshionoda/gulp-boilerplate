5cebed983857e4589687524e0d9e8b0f32bbc0ee
Satoshi Onoda <satoshionoda@gmail.com>
2020-11-12 20:44:34 +0900

    jest 導入
    jasmine-fixtureは諦めた

--- src/ts/test/mytest.spec.ts
+++ src/ts/test/mytest.spec.ts
@@ -1,14 +1,7 @@
 import { MainDoc } from "../app/MainDoc";
 
 describe("MainDoc", () => {
-  beforeEach(() => {
-    loadFixtures("products/index.html");
-  });
-  it("should be created", () => {
+  test("should be created", () => {
     expect(new MainDoc()).toBeTruthy();
-  });
-  it("should find fixture", () => {
-    const result = $("h1").text();
-    expect(result).toBe("HELLO WORLD");
   });
 });
