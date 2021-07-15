c384f8bd838cf1d76ebbbf8fece174bb00debf4c
Satoshi Onoda <satoshionoda@gmail.com>
2021-07-15 14:46:22 +0900

    lint 設定変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,8 @@ module.exports = {
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
   },
 };
